4c02fe11f1b6452cbab3212c735330b71342a55a
Timur Saliakhutdinov <t.salyakhutdinov@gmail.com>
2018-02-07 18:30:02 +0300

    Add test for 'option when' case

--- src/test/groovy/com/github/timic/vavr/groovy/VavrStaticExtensionSpec.groovy
+++ src/test/groovy/com/github/timic/vavr/groovy/VavrStaticExtensionSpec.groovy
@@ -1,6 +1,7 @@
 package com.github.timic.vavr.groovy
 
 import io.vavr.collection.Stream
+import io.vavr.control.Option
 import spock.lang.Specification
 
 class VavrStaticExtensionSpec extends Specification {
@@ -14,6 +15,16 @@ class VavrStaticExtensionSpec extends Specification {
     void "stream continually"() {
         expect:
             Stream.continually { 1 }.take(3) == Stream.of(1, 1, 1)
+    }
+
+    void "option when"() {
+        when:
+            def opt = Option.when(true) {
+                1
+            }
+        then:
+            opt.defined
+            opt.get() == 1
     }
 
 }
